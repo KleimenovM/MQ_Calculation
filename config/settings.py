28fb1272e0e8d166da3884d555dfb079a7c106a0
KleimenovM <kleimenov.mi@phystech.edu>
2025-04-29 15:22:51 +0200

    spectrum plotter uploaded

--- config/settings.py
+++ config/settings.py
@@ -7,3 +7,8 @@ ROOT_DIR = Path(__file__).resolve().parent.parent
 
 DATA_DIR = os.path.join(ROOT_DIR, 'data')
 PICS_DIR = os.path.join(ROOT_DIR, 'pics')
+
+SPECTRUM_DIR = os.path.join(DATA_DIR, 'spectrum')
+ELECTRONS_DIR = os.path.join(DATA_DIR, 'electrons')
+PROTONS_DIR = os.path.join(DATA_DIR, 'protons')
+ISRF_DIR = os.path.join(DATA_DIR, 'isrf')
